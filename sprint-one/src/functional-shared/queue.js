acb6a984b02b51deb70cf2cc12bb1a525c14062b
kidmillions <claymanchris@gmail.com>
2015-05-13 16:00:24 -0700

    finished function-shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,18 +1,36 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = {};
-  _.extend(someInstance, queueMethods);
+  var someInstance = {
+    storage: {},
+    start: 0,
+    end: 0
+  };
+  extend(someInstance, queueMethods);
   return someInstance;
 };
-
+var extend = function(to, from) {
+  for(var key in from) {
+    to[key] = from[key];
+  }
+};
 var queueMethods = {
-  length: 0,
+
   size: function(){
-    return this.length;
+    var size = this.end - this.start;
+    if(size < 0) {
+      size = 0;
+    }
+    return size;
   },
-  push: function(value) {
-    this.length++;
+  enqueue: function(value) {
+    this.storage[this.end] = value;
+    this.end++;
+  },
+  dequeue: function() {
+    var result = this.storage[this.start];
+    this.start++;
+    return result;
   }
 };
 
