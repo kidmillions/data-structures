fe131328e2973d6e4557c47dfd6ff772b4399b71
kidmillions <claymanchris@gmail.com>
2015-05-13 15:18:31 -0700

    finished functional shared patter stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,20 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  length: 0,
+  size: function(){
+    return this.length;
+  },
+  push: function(value) {
+    this.length++;
+  }
+};
 
 
 
