d46858728aa8c54bf532f58b456302673fa88846
kidmillions <claymanchris@gmail.com>
2015-05-15 11:56:15 -0700

    linkedList spec complete.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,17 +4,27 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-    var oldValue = this.tail;
-    this.tail = Node(value);
+    // var oldValue = this.tail;
+    // this.tail = Node(value);
 
-    //add node to tail
+    // //add node to tail
+    // if (this.head === null) {
+    //   this.head = ;
+    // } else {
+    //   this[oldValue].next = this.tail;
+    // }
+    // //store previous tail
+    //   //point it to new tail
+    var newNode = Node(value);
+
     if (this.head === null) {
-      this.head = ;
+      this.head = newNode;
     } else {
-      this[oldValue].next = this.tail;
+      this.tail.next = newNode;
     }
-    //store previous tail
-      //point it to new tail
+
+    this.tail = newNode;
+
 
   };
 
@@ -22,15 +32,21 @@ var LinkedList = function(){
     var head = this.head.value;
     this.head = this.head.next;
     return head;
+
   };
 
   list.contains = function(target){
-    for (var key in this) {
-      if (key.value === target) {
+
+    var search = function(node) {
+      if (node.value === target) {
         return true;
+      } else if (node.next === null) {
+        return false;
+      } else {
+        return search(node.next);
       }
-    }
-    return false;
+    };
+    return search(this.head);
   };
 
   return list;
