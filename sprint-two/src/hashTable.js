2f8f5c8a62248ca1c4313a62759cd5495990fc20
kidmillions <claymanchris@gmail.com>
2015-05-15 16:42:18 -0700

    Continued work on hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,33 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //create object
+  var obj = {k: v};
+
+
+    //check to see if index is null
+    if (this.retrieve(i) === null) {
+
+    //if null
+      //insert/set object
+      this._storage.set(i, obj);
+    }
+
+    //if !null
+      //retrieve object that already exists at index
+      //iterate / extend new object into old one
+      //
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  return this._storage.get(i)[k];
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i[k], null);
 };
 
 
