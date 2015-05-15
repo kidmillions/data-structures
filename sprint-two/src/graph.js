6b6881a9d54fcf8b4a3a7b03d5068e36716a7f07
kidmillions <claymanchris@gmail.com>
2015-05-15 15:37:14 -0700

    fixed graph test spec; completed graph tests.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,46 @@
 
 
 var Graph = function(){
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.nodes[node] = {value:node, edges: {}};
 };
 
 Graph.prototype.contains = function(node){
+    //if nodes contains node
+  for (var key in this.nodes) {
+    if (key === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.nodes[node];
+    //delete node
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return _.contains(this.nodes[fromNode].edges, toNode);
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.nodes[fromNode].edges[toNode] = toNode;
+  this.nodes[toNode].edges[fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  delete this.nodes[fromNode].edges[toNode];
+  delete this.nodes[toNode].edges[fromNode];
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this.nodes) {
+    cb(key);
+  }
 };
 
 /*
