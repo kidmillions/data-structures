2ac55729d05e4cc13f5fcaef1f37787e14e829d0
kidmillions <claymanchris@gmail.com>
2015-05-15 12:19:50 -0700

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
+
 
   return newTree;
 };
@@ -15,10 +17,35 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
+
+
+
 treeMethods.contains = function(target){
+
+  var containsTarget = false;
+
+
+
+
+  var searchTree = function(node) {
+    if (node.value === target) {
+      containsTarget = true;
+    } else if (node.children.length === 0) {
+      return;
+    } else {
+      //loop through node's children
+      for (var i = 0; i < node.children.length; i++) {
+        searchTree(node.children[i]);
+        //search the tree recursively for each child
+      }
+    }
+  };
+
+  searchTree(this);
+  return containsTarget;
 
 };
 
