e2f873e2eff0dd9aed3d3b4ecaf92c3d6934640d
kidmillions <claymanchris@gmail.com>
2015-05-16 16:28:16 -0700

    Transformizfied duples from objects into arrays in hash table.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,40 +5,42 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this.limit);
-
-    var bucket = [];
-    var tuple = [k,v];
-    bucket.push(tuple);
-    this._storage.set(i, bucket);
+  var bucket = [];
+  var tuple = [k, v];
+  if (this._storage.get(i)) {
+    bucket = this._storage.get(i);
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+        this._storage.set(i, bucket);
+        return;
+      }
+    }
+  }
+  bucket.push(tuple);
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this.limit);
-  // //get bucket
   var bucket = this._storage.get(i);
   for(var i=0; i<bucket.length;i++) {
     if(bucket[i][0] === k) {
       return bucket[i][1];
-    };
+    }
   }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-  var i = getIndexBelowMaxForKey(k, this.limit);
-  var bucket = this._storage.get(i);
-
-  // this._storage.each(function(bucket) {
-  //   for(var i = 0;i<bucket.length;i++) {
-  //     if(bucket[i][0] === k) {
-  //       bucket[i][1] = null;
-  //       var result = bucket;
-  //               console.lot(result);
-
-  //     }
-  //   }
-  // })
-  this_.storage.set(i, result);
-
+  var index = getIndexBelowMaxForKey(k, this.limit);
+  var bucket = this._storage.get(index);
+  for(var  i= 0;i<bucket.length;i++) {
+    if(bucket[i][0] === k) {
+      bucket[i][1] = null;
+    }
+  }
+  this._storage.set(index, bucket);
 };
 
 
