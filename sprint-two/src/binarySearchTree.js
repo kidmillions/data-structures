8968184646a7994ad9d38001b4226f01ef4b5326
kidmillions <claymanchris@gmail.com>
2015-05-15 20:00:45 -0700

    work on binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,49 @@
 var BinarySearchTree = function(value){
+  var tree = Object.create(bTreeMethods);
+  tree.value = value;
+  return tree;
 };
+
+  var bTreeMethods = {};
+  bTreeMethods.insert = function(value) {
+    if(value < this.value) {
+      if (this.left === undefined) {
+        this.left = BinarySearchTree(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if(this.right === undefined) {
+        this.right = BinarySearchTree(value);
+      } else {
+        this.right.insert(value);
+      }
+
+    };
+  };
+  bTreeMethods.contains = function(target) {
+    var containsTarget = false;
+
+    var search = function(node) {
+      debugger;
+      if (node.value === target) {
+        containsTarget = true;
+      } else if (node.value < target) {
+        return search(node.right);
+      } else {
+        return search(node.left);
+      }
+    }
+
+    search(this);
+    return containsTarget;
+
+  };
+  bTreeMethods.depthFirstLog = function(){
+
+  };
+
+
 
 
 /*
